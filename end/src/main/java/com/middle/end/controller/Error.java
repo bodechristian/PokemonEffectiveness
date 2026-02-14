083363d451bf579f31ab2fa92c3e8cae74533de1
Christian Bode <christianbode97@web.de>
2026-02-14 23:54:15 +0100

    added openapi and created tasks to generation client code

--- end/src/main/java/com/middle/end/controller/Error.java
+++ end/src/main/java/com/middle/end/controller/Error.java
@@ -1,10 +1,12 @@
 package com.middle.end.controller;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.boot.webmvc.error.ErrorController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Hidden
 public class Error implements ErrorController {
 
     @GetMapping("/error")
