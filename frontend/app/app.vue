7c005bbacbfb2d7c774206cd7e3a572a330193ee
Christian Bode <christianbode97@web.de>
2026-02-15 11:54:36 +0100

    use newly generated client code in frontend and fix cors issue

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button @click="fetchGreeting">Get Greeting</button>
+    <p>{{ greeting }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+import {useHelloWorld} from "~~/composables/useHelloWorld";
+
+const { getGreeting } = useHelloWorld();
+
+const greeting = ref<string>('nuttin');
+
+const fetchGreeting = async () => {
+  greeting.value = await getGreeting();
+};
+</script>
